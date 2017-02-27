be4102c882f59249f80d43c4f1bdecfbd90ddd05
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2017-02-27 14:53:45 -0300

    Mais exercicio

--- src/stringTestes/Test.java
+++ src/stringTestes/Test.java
@@ -9,6 +9,13 @@ public class Test {
 		for (int i = 0; i < s.length(); i++) {
 			System.out.println(s.charAt(i));
 		}
+		
+		String s1 = "Socorram-me, subi no ônibus em Marrocos";
+		String s2 = "anotaram a data da maratona";
+		
+		for (int i = s2.length(); i > 1; i--) {
+			System.out.println(s2.charAt(i-1));
+		}
 
 	}
 
