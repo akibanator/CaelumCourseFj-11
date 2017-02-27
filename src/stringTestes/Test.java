696bff88775bf15a12bba7efb097c4063dfc330d
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2017-02-27 14:43:06 -0300

    String para teste

---
+++ src/stringTestes/Test.java
@@ -0,0 +1,15 @@
+package stringTestes;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String s = "String para testar charAt e lenght";
+		
+		for (int i = 0; i < s.length(); i++) {
+			System.out.println(s.charAt(i));
+		}
+
+	}
+
+}
